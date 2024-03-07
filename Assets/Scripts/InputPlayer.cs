9c3cd750e7fe683a27e01e74e901339112d0d00d
Omarchg <omarchaglez@gmail.com>
2024-03-07 09:47:19 +0000

    Examen completo

--- Assets/Scripts/InputPlayer.cs
+++ Assets/Scripts/InputPlayer.cs
@@ -1,18 +1,91 @@
+using TMPro;
 using UnityEngine;
 
 public class InputPlayer : MonoBehaviour
 {
     public Pool pool;
-    public Transform spawnTransform; // Transform de spawn para las balas
+    public Transform spawnTransform; // Posicion donde aparecen las balas
     [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private float bulletCooldown = 0.5f;
+    private float bulletTime;
+    [SerializeField] private float bulletReload = 15f;
+    bool bulletAutoReload = false;
+    bool bulletPlayerReload = false;
+    private float bulletReloadAux;
+    [SerializeField] private float bulletRechargeAuto = 2f;
+    [SerializeField] private float bulletRechargePlayer = 1f;
+    private float bulletReloadAutoTime = 0f;
+    private float bulletReloadPlayerTime = 0f;
+    public TextMeshProUGUI BulletText;
 
-    
+
+    private void Start()
+    {
+        bulletTime = bulletCooldown; // Pa que pueda disparar desde el principio
+        bulletReloadAux = bulletReload;
+    }
+
     void Update()
     {
-        if (Input.GetButtonUp("Fire1"))
+        if (Input.GetButtonUp("Fire1") && bulletTime >= bulletCooldown && (bulletPlayerReload == false))
         {
-            FireBullet();
+            if (bulletReload >= 1)
+            {
+                FireBullet();
+                bulletTime = 0;
+                bulletReload -= 1;
+            }
+            else
+            {
+                bulletAutoReload = true;
+            }
         }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            bulletPlayerReload = true;
+        }
+
+        bulletTime += Time.deltaTime;
+        
+        if (bulletAutoReload)
+        {
+            bulletReloadAutoTime += Time.deltaTime;
+            BulletText.text = "Recargando";
+            
+        }
+
+        if (bulletPlayerReload)
+        {
+            bulletReloadPlayerTime += Time.deltaTime;
+            
+            BulletText.text = "Recargando";
+
+        }
+
+        if (bulletReloadPlayerTime >= bulletRechargePlayer)
+        {
+            bulletPlayerReload = false;
+            bulletReloadPlayerTime = 0f;
+            bulletReload = bulletReloadAux;
+            
+        }
+
+        if (bulletReloadAutoTime >= bulletRechargeAuto)
+        {
+            bulletAutoReload = false;
+            bulletReloadAutoTime = 0f;
+            bulletReload = bulletReloadAux;
+            
+        }
+
+
+        if (bulletAutoReload == false && bulletPlayerReload == false)
+        {
+            BulletText.text = bulletReload + "/" + bulletReloadAux;
+        }
+        
+
     }
 
     void FireBullet()
